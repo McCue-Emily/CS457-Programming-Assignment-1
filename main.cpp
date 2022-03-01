c75edfd24ab84c45594545fc36c684531840f910
Emily McCue <emily@msys.ca>
2022-03-01 10:01:55 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 
 void tokenize(char* tokens);
-
 void create();
 void drop();
 void use();
@@ -23,9 +22,7 @@ int main() {
 
         cout << "--CS457 PA1" << endl << endl;
         cin.getline(userInput, 50);
-
         char* tokens = strtok(userInput, " ");
-
         tokenize(tokens);
 
         exit = true;                
@@ -38,18 +35,41 @@ int main() {
 void tokenize(char* tokens) {
 
         cout << "token1 = " << tokens << endl;
+        token1 = tokens;
         
         tokens = strtok(NULL, " ");
         cout << "token2 = " << tokens << endl;
+        token2 = tokens;
         
         tokens = strtok(NULL, " ");
         cout << "token3 = " << tokens << endl;
+        token3 = tokens;
 
         tokens = strtok(NULL, " ");
         cout << "token4 = " << tokens << endl;
+        token4 = tokens;
 
         tokens = strtok(NULL, " ");
         cout << "token5 = " << tokens << endl;
+        token5 = tokens;
+
+        switch (token1)
+        {
+        case (token1 == "CREATE"):
+            /* code */
+            break;
+        case (token1 == "DROP"):
+            break;
+        case (token1 == "USE"):
+            break;
+        case (token1 == "ALTER"):
+            break;
+        case (token1 == "SELECT"):
+            break;
+        default:
+            cout << "invalid input."
+            break;
+        }
 
 }
 
